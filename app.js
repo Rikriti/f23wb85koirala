851b135d197ee71d8e2e7ab8c8a28518016664f9
Rikriti Koirala <rikritikoirala@gmail.com>
2023-11-01 19:50:47 -0500

    evreything upto date

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var elephantRouter=require('./routes/elephant');
+var boardRouter=require('./routes/board');
+var ChooseRouter = require('./routes/choose');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/elephant',elephantRouter);
+app.use('/board',boardRouter);
+app.use('/choose', ChooseRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
